569a685b128d9a20af902de5c112327db3c6d38d
jhpoelen <jhpoelen@gmail.com>
2016-12-06 15:08:57 -0800

    tabs

--- test/tests.cc
+++ test/tests.cc
@@ -1,12 +1,11 @@
-// tests.cpp
 #include "../ipc.c"
 #include <gtest/gtest.h>
 
 TEST(IPCTest, RemoveNonExistentClient) {
   ASSERT_EQ(-1, remove_uclient(0));
 }
- 
+
 int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
